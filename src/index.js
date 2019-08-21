13332e4a0fb55be939614ef98dc6065ec9ac8fb1
ryosuken <ryosuke_wsd611@akane.waseda.jp>
2019-08-21 11:29:19 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import MemoStore, { memoReducer } from './memo/Store'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux Persistの設定
+const persistConfig = {
+  key: 'memo',
+  storage,
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// persist reducerの作成
+const persistedReducer = persistReducer(persistConfig, memoReducer);
+
+// store, persisterの作成
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
+
+// 表示をレンダリング
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+    <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
+
+export default pstore;
